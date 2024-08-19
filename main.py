399d5fdb36e4a21cd0482af559e69d790d5dbfc3
LinlyBoi <linlysmolworthy@gmail.com>
2024-08-20 00:22:31 +0300

    Real time video with original comparison window

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ cv2.namedWindow("canny filter")
 if cam.read() == False:
     cam.open()
 
-img_counter = 0
 
 while True:
     ret, image = cam.read()
@@ -17,16 +16,17 @@ while True:
 
 
         # SPACE pressed
-    if cv2.waitKey(1) & 0xFF == ord('k'):
-        img_name = "canny_image.png"
-        grayed_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blurred_image = cv2.GaussianBlur(grayed_image, (5, 5), 0)
-        canny_image = cv2.Canny(image, 105, 200)
-        cv2.imwrite(img_name, canny_image)
+    # if cv2.waitKey(1) & 0xFF == ord('k'):
+    img_name = "canny_image.png"
+    grayed_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(grayed_image, (5, 5), 0)
+    canny_image = cv2.Canny(image, 105, 200)
+    cv2.imshow("original", image)
+    cv2.imshow("filtered", canny_image)
+    # cv2.imwrite(img_name, canny_image)
+        # break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cam.release()
-cv2.imshow("canny filter applied", canny_image)
-cv2.waitKey(1)
-
 cv2.destroyAllWindows()
